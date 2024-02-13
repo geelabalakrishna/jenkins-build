62cff8c0ec2cc36bf27a1be3cba1ea860af52e3a
Balakrishna.G <70707659+geelabalakrishna@users.noreply.github.com>
2024-02-13 17:58:20 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,13 +1,14 @@
 #!/bin/bash #shebang
-cd /tmp/
+cd /$HOME/
 rm -rf /tmp/apache-maven*
 wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 APACHE=$(ls -l | grep apache-maven | awk '{print $9}')
 tar vxfz $APACHE
 rm -rf $APACHE
 MAVEN_DIR=$(ls -ltr | grep apache-maven | awk '{print $9}')
-export MAVEN_HOME=/tmp/$MAVEN_DIR
-export PATH=$MAVEN_HOME/bin:$PATH
+echo "export MAVEN_HOME=/$HOME/$MAVEN_DIR" >> $HOME/.bash_profile
+echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> $HOME/.bash_profile
+source $HOME/.bash_profile
 echo "maven install sucessfully"
 mvn --version
 env
