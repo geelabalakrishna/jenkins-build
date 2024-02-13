d3aac52cd29c136cadb9076b6144422596f22cda
Balakrishna.G <70707659+geelabalakrishna@users.noreply.github.com>
2024-02-13 17:44:24 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,5 +1,6 @@
 #!/bin/bash #shebang
 cd /tmp/
+rm -rf /tmp/apache-maven*
 wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 APACHE=$(ls -l | grep apache-maven | awk '{print $9}')
 tar vxfz $APACHE
